58c0254e80357fc83b0b49582eb826e24a96baf7
364864264@qq.com <364864264@qq.com>
2019-12-24 22:50:26 +0800

    提交

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {regexpValidator, AsyncValidate, checkPasswordConfirm} from '../shared/validate.directive';
 
 @Component({
   selector: 'app-message',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.component.less']
 })
 export class MessageComponent implements OnInit {
+  formData = {
+    user: '',
+    username: '',
+    password: '',
+    passwordConfirm: ''
+  };
 
-  constructor() { }
+  myForm: FormGroup;
 
   ngOnInit() {
+    this.myForm = new FormGroup({
+      user: new FormControl(this.formData.user, {
+          validators: Validators.required,
+          asyncValidators: [this.asy.validate.bind(this.asy)],
+          updateOn: 'blur'
+        }
+      ),
+      username: new FormControl(this.formData.username, [
+        Validators.required,
+        regexpValidator('account', '用户名')
+      ]),
+      password: new FormControl(this.formData.password, [
+        Validators.required,
+        regexpValidator('password', '登录密码')
+      ]),
+      passwordConfirm: new FormControl(this.formData.passwordConfirm, [
+        Validators.required,
+        // confirmPassword('password')
+      ])
+    }, {validators: [checkPasswordConfirm]});
   }
 
+  get user() {
+    return this.myForm.get('user');
+  }
+
+  get username() {
+    return this.myForm.get('username');
+  }
+
+  get password() {
+    return this.myForm.get('password');
+  }
+
+  get passwordConfirm() {
+    return this.myForm.get('passwordConfirm');
+  }
+
+  constructor(private asy: AsyncValidate) {
+  }
 }
