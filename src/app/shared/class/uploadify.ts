2595ffe031c307a12d7cdc9f0ed542b7220ea6e8
364864264 <364864264@qq.com>
2019-12-31 16:38:25 +0800

    提交

--- src/app/shared/class/uploadify.ts
+++ src/app/shared/class/uploadify.ts
@@ -2,47 +2,47 @@ import {Utils} from './utils';
 
 export class Uploadify {
   private utils = new Utils();
-  private conf = {
-    list: '', // 显示文件容器
-    type: '', // 上传显示的列表 image,file显示图片或文件
-    swf: './assets/webuploader/Uploader.swf',
-    server: 'http://www.api.me/index/index/upload',
-    dnd: undefined, // 指定Drag And Drop拖拽的容器
-    disableGlobalDnd: false, // 是否禁掉整个页面的拖拽功能
-    paste: undefined, // 粘贴来添加截屏的图片 指定监听paste事件的容器 建议设置为document.body
-    auto: true,
-    pick: {
-      id: '#picker',
-      // label: '', // 请采用 innerHTML 代替
-      innerHTML: '上传图片', // 指定按钮文字
-      multiple: false // 是否开起同时选择多个文件能力
-    },
-    compress: false, // 图片在上传前不进行压缩
-    headers: {},
-    accept: {
-      title: 'Images', // 文字描述
-      extensions: 'gif,jpg,jpeg,bmp,png', // 允许的文件后缀，不带点，多个用逗号分割。
-      mimeTypes: 'image/!*' // 多个用逗号分割。
-    },
-    formData: {},
-    fileVal: 'file',
-    method: 'POST',
-    fileNumLimit: undefined, // 验证文件总数量
-    fileSizeLimit: undefined, // 验证文件总大小是否超出限制
-    fileSingleSizeLimit: 1024 * 1024 * 2, // 验证单个文件大小是否超出限制
-    uploadFinished: undefined, // 所有文件上传结束后触发
-    uploadSuccess: undefined, // 上传成功后触发
-    uploadComplete: undefined // 不管成功或者失败，文件上传完成时触发
-  };
   create(opts) {
-    Object.assign(this.conf, opts);
+    const conf = {
+      list: '', // 显示文件容器
+      type: '', // 上传显示的列表 image,file显示图片或文件
+      swf: './assets/webuploader/Uploader.swf',
+      server: 'http://www.api.me/index/index/upload',
+      dnd: undefined, // 指定Drag And Drop拖拽的容器
+      disableGlobalDnd: false, // 是否禁掉整个页面的拖拽功能
+      paste: undefined, // 粘贴来添加截屏的图片 指定监听paste事件的容器 建议设置为document.body
+      auto: true,
+      pick: {
+        id: '#picker',
+        // label: '', // 请采用 innerHTML 代替
+        innerHTML: '上传图片', // 指定按钮文字
+        multiple: false // 是否开起同时选择多个文件能力
+      },
+      compress: false, // 图片在上传前不进行压缩
+      headers: {},
+      accept: {
+        title: 'Images', // 文字描述
+        extensions: 'gif,jpg,jpeg,bmp,png', // 允许的文件后缀，不带点，多个用逗号分割。
+        mimeTypes: 'image/!*' // 多个用逗号分割。
+      },
+      formData: {},
+      fileVal: 'file',
+      method: 'POST',
+      fileNumLimit: undefined, // 验证文件总数量
+      fileSizeLimit: undefined, // 验证文件总大小是否超出限制
+      fileSingleSizeLimit: 1024 * 1024 * 2, // 验证单个文件大小是否超出限制
+      uploadFinished: undefined, // 所有文件上传结束后触发
+      uploadSuccess: undefined, // 上传成功后触发
+      uploadComplete: undefined // 不管成功或者失败，文件上传完成时触发
+    };
+    Object.assign(conf, opts);
     const self = this;
-    const upload = new WebUploader.create(self.conf);
+    const upload = new WebUploader.create(conf);
     // 当文件被加入队列之前触发
     upload.on('fileQueued', (file) => {
       let $list;
       // 图片列表
-      if (self.conf.type === 'image') {
+      if (conf.type === 'image') {
         $list = $('<div id="' + file.id + '" class="image-item fid="">' +
           '<img><div class="image-panel">' +
           '<span class="data"></span>' +
@@ -67,7 +67,7 @@ export class Uploadify {
         }, thumbnailWidth, thumbnailHeight);
       }
       // 文件列表
-      if (self.conf.type === 'file') {
+      if (conf.type === 'file') {
         $list = $('<div class="uploadIfy-queue-item" id="' + file.id + '" fid="">' +
           '<div class="cancel"><a href="javascript:void(0);">X</a></div>' +
           '<span class="fileName">' + file.name + ' (' + self.utils.bytesToSize(file.size) + ')</span>' +
@@ -75,9 +75,9 @@ export class Uploadify {
           '<div class="uploadIfy-progress-bar"></div>' +
           '</div></div>');
       }
-      if (self.conf.list && self.conf.type) {
+      if (conf.list && conf.type) {
         // $list为容器jQuery实例
-        $(self.conf.list).append($list);
+        $(conf.list).append($list);
         // 删除文件
         $list.on('click', '.cancel', () => {
           const fid = $list.attr('fid');
